7955d925186906f3c4ea8187d028034fce520f95
farsidd <farhanfsd.pk@gmail.com>
2022-12-09 21:13:03 +0500

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
+<template>
+  <div class="home">
+    <button @click="decreaseCounter" class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button @click="increaseCounter" class="btn">+</button>
+  </div>
+</template>
+//new recommended way to use setup
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+const counter = ref(0);
+const increaseCounter = () => {
+  counter.value++;
+};
+const decreaseCounter = () => {
+  counter.value--;
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.home {
+  text-align: center;
+  padding-top: 50px;
+}
+.btn,
+span {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
